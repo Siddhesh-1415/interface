b08023061dbb5107d55f9c51da6cf3e1a26d79f4
TAMBE SIDDHESH BABAN <tambesiddhesh1@gmail.com>
2025-06-07 12:50:41 +0530

    Create App.jsx

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+react se bnaya dekhe ha
+import Movies from "./Moviezone/Movies";
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+    <Movies />
+    </>
+  );
+};
+
+export default App;
